6cccbcdbc778b45c96cdfce641f7947f1dffd9ab
Moenes98 <amunes.f98@gmail.com>
2025-05-15 21:01:27 +0300

    Add repository interfaces

--- Infrastructure/GlobalUsings.cs
+++ Infrastructure/GlobalUsings.cs
@@ -2,4 +2,7 @@
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.EntityFrameworkCore;
+global using Domain.Interfaces;
+
+
 
