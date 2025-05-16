d61668a12b99399eb0a0ddc77799a9ec14dd4bd2
HazemRashad <hazemrashad55@gmail.com>
2025-05-17 01:07:27 +0300

    Implemented Area Services according to the repository.

--- Application/Services/AreaService.cs
+++ Application/Services/AreaService.cs
@@ -1,4 +1,6 @@
-﻿namespace Application.Services
+﻿using Domain.Entities;
+
+namespace Application.Services
 {
     public class AreaService : IAreaService
     {
@@ -11,27 +13,50 @@
 
         public async Task<IEnumerable<AreaDto>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            IEnumerable<Area> areas = await _areaRepo.GetAllAsync();
+            return areas.Select(area => new AreaDto
+            {
+                AreaId = area.AreaId,
+                AreaName = area.AreaName
+            });
         }
 
         public async Task<AreaDto> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            Area area = await _areaRepo.GetByIdAsync(id);
+            return new AreaDto()
+            {
+                AreaId = area.AreaId,
+                AreaName = area.AreaName
+            };
         }
 
         public async Task CreateAsync(AreaDto dto)
         {
-            throw new NotImplementedException();
+            var area = new Area
+            {
+                AreaId = dto.AreaId,
+                AreaName = dto.AreaName
+            };
+            await _areaRepo.AddAsync(area);
+            await _areaRepo.SaveChangesAsync();
         }
 
         public async Task UpdateAsync(int id, AreaDto dto)
         {
-            throw new NotImplementedException();
+            Area area = await _areaRepo.GetByIdAsync(id);
+            area.AreaId = dto.AreaId;
+            area.AreaName = dto.AreaName;
+
+            _areaRepo.Update(area);
+            await _areaRepo.SaveChangesAsync();
         }
 
         public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            Area area = await _areaRepo.GetByIdAsync(id);
+            _areaRepo.Delete(area);
+            await _areaRepo.SaveChangesAsync();
         }
     }
 }
