10e9061f58da92619af674c5b8760b6b5c9f4a99
HazemRashad <hazemrashad55@gmail.com>
2025-05-17 00:45:35 +0300

    added service interfaces for each of Area and comment.

---
+++ Application/Interfaces/IAreaService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Application.Interfaces
+{
+    public interface IAreaService
+    {
+        Task<IEnumerable<AreaDto>> GetAllAsync();
+        Task<AreaDto> GetByIdAsync(int id);
+        Task CreateAsync(AreaDto dto);
+        Task UpdateAsync(int id, AreaDto dto);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Application/Interfaces/ICommentService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Application.Interfaces
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<CommentDto>> GetAllAsync();
+        Task<CommentDto> GetByIdAsync(int id);
+        Task CreateAsync(CommentDto dto);
+        Task UpdateAsync(int id, CommentDto dto);
+        Task DeleteAsync(int id);
+    }
+}
