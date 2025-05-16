99fdb4c9f34ccbf5203437d3ed4d86ba5e22c6f7
HazemRashad <hazemrashad55@gmail.com>
2025-05-17 01:37:42 +0300

    Added Area and Comment configuration.

---
+++ Infrastructure/Data/Configurations/AreaConfiguration.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Infrastructure.Data.Configurations
+{
+    public class AreaConfiguration : IEntityTypeConfiguration<Area>
+    {
+        public void Configure(EntityTypeBuilder<Area> builder)
+        {
+            builder.HasKey(x => x.AreaId);
+            builder.Property(p => p.AreaName);
+        }
+    }
+}
---
+++ Infrastructure/Data/Configurations/CommentConfiguration.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Infrastructure.Data.Configurations
+{
+    public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+    {
+        public void Configure(EntityTypeBuilder<Comment> builder)
+        {
+            builder.HasKey(x => x.CommentId);
+            builder.Property(p => p.Content).IsRequired();
+
+            builder.HasOne(x => x.Issue)
+                .WithMany(i => i.Comments)
+                .HasForeignKey(x => x.IssueId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
