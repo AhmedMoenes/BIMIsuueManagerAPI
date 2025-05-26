e02a982dc199c50072ef12b7e00416918b962a92
mostafaelwahsh <mostafanabil447@gmail.com>
2025-05-26 20:07:22 +0300

    #Adding Login Validation

---
+++ Application/Validators/Login/LoginRequestDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators.Login
+{
+    internal class LoginRequestDtoValidator : AbstractValidator<DTOs.Login.LoginRequestDto>
+    {
+        public LoginRequestDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email format.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters long.")
+                .MaximumLength(100).WithMessage("Password must not exceed 100 characters.");
+        }
+    }
+}
